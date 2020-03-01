328b1d1c228b455df09122d8626e81a39f66353c
nanbingjun <nanbingjun@gmail.com>
2020-03-02 02:13:29 +0900

    first init

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { UserEditComponent } from './user/user-edit/user-edit.component';
+import { ConfirmDialogComponent } from './tools/confirm-dialog/confirm-dialog.component';
+
+import { UserService } from './user/user.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    DashboardComponent,
+    UserListComponent,
+    UserEditComponent,
+    ConfirmDialogComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    MatDialogModule,
+    HttpClientModule,
+    BrowserAnimationsModule
+  ],
+  providers: [
+    UserService
+  ],
+  entryComponents: [
+    ConfirmDialogComponent
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ my-app/src/app/user/user.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { User } from './user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  users: User[];
+  private url = 'http://127.0.0.1:8000';
+
+  constructor( private http: HttpClient) {
+    this.users = [
+      { id: 1, name: 'Yamada', email: 'yamada@example.com' },
+      { id: 2, name: 'Suzuki', email: 'suzuki@example.com' },
+      { id: 3, name: 'Tanaka', email: 'tanaka@example.com' },
+    ];
+  }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.url}/users`)
+      .pipe(
+        catchError(this.handleError('getUsers', []))
+      );
+  }
+
+  getUser(id: number): Observable<User> {
+      return this.http.get<User>(`${this.url}/users/${id}`)
+        .pipe(
+          catchError(this.handleError<User>(`getUser id=${id}`))
+        );
+  }
+
+  setUser(user: User): Observable<User> {
+    const id = user.id;
+    return this.http.post<User>(`${this.url}/users/${id}`, user)
+      .pipe(
+        catchError(this.handleError<User>(`setUser id=${id}`))
+      );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      console.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
+  }
+
+}
